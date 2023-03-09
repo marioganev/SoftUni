62a578d93c496e308cf9c17355ad582f5ea46482
Mario Ganev <82400980+marioganev@users.noreply.github.com>
2023-03-09 18:53:14 +0200

    Add files via upload

---
+++ Programming Basics with C#/07. For Loop - Lab/01. Numbers from 1 to 100.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace NumsFrom1to100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num <= 100; num++)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/07. For Loop - Lab/02. Numbers N...1.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace NumsFromNto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
